1a64cfe38297eb0677e4558d292c08f0cfc8522e
zhangfengzhou <1120335370@qq.com>
2019-11-25 13:33:42 +0800

    实现百度语音插件功能

---
+++ android/app/src/main/java/io/flutter/plugins/AsrPluginRegistrant.java
@@ -0,0 +1,25 @@
+package io.flutter.plugins;
+
+
+import com.example.asr_plugin.AsrPlugin;
+
+import io.flutter.plugin.common.PluginRegistry;
+
+public class AsrPluginRegistrant {
+
+    public static void registerWith(PluginRegistry registry) {
+        if (alreadyRegisteredWith(registry)) {
+            return;
+        }
+        AsrPlugin.registerWith(registry.registrarFor("com.example.asr_plugin.AsrPlugin"));
+    }
+
+    private static boolean alreadyRegisteredWith(PluginRegistry registry) {
+        final String key = GeneratedPluginRegistrant.class.getCanonicalName();
+        if (registry.hasPlugin(key)) {
+            return true;
+        }
+        registry.registrarFor(key);
+        return false;
+    }
+}
--- android/app/src/main/kotlin/com/example/flutter_trip/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_trip/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.flutter_trip
 
 import android.os.Bundle
+import com.example.asr_plugin.AsrPlugin
 
 import io.flutter.app.FlutterActivity
+import io.flutter.plugins.AsrPluginRegistrant
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
+    GeneratedPluginRegistrant.registerWith(this);
+    // 注册Asr插件
+    AsrPluginRegistrant.registerWith(this);
   }
-  
-  
   
 }
